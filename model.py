e852c6687e3b77dc64e44165d0d3d6ffc1439501
hsk9767 <34028332+hsk9767@users.noreply.github.com>
2019-12-09 15:55:46 +0900

    Update model.py

--- model.py
+++ model.py
@@ -39,7 +39,7 @@ import torch.nn as nn
 
 ##새로운 구조 -> 8분 에 97%
 ##새로운 구조를 하는데, x_1 은 이미 2048이니까 2048 로 가는 Linear 하지 않고 해 보기. -> 시간이 너무 느렸음.
-##새로운 구조를 하는데 batch size 3 -> 
+##새로운 구조를 하는데 batch size 3 -> 걸린 시간 : 4m31s, acc : 0.9678 %
 
 class convnet(nn.Module):
     def __init__(self):
