85e168b7307ebeb74192472271994d91090e77ae
hsk9767 <34028332+hsk9767@users.noreply.github.com>
2019-12-10 21:19:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ val_dir = './npy_val'
 train_dataset = FontDataset(train_dir)
 val_dataset = FontDataset(val_dir)
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size = b_size, shuffle=True)
+                                           batch_size = b_size, shuffle=False)
 
 val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
                                          batch_size = 1)
